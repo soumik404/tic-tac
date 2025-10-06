79ea9f52e923ea80ff16cdff238600921e392eea
Soumik <soumik.ghosh@sit4u.net>
2025-10-06 21:34:07 +0530

    Updated metadata and tags for Tic-Tac-Toe

--- minikit.config.ts
+++ minikit.config.ts
@@ -14,30 +14,32 @@ export const minikitConfig = {
     "payload": "eyJkb21haW4iOiJ0aWMtdGFjLXBlYXJsLnZlcmNlbC5hcHAifQ",
     "signature": "MHg1NTU3NTBlYjcxOGNmMTI1ODFiOGFiOTk2YTIxOTgwN2EyMGVkN2ZhYTRiNWIzN2NkMTE4ODVlODA3M2JjYjZiNmJlNWE5OGFiNDg3MDMzYTJmZDkzNjAwMjU5ZGY4Y2FhY2E5NmE0NDM3MDExMDExNDMyYmE0YzUwODliMWViNTFj"
   },
-  "baseBuilder": {
-    "allowedAddresses": [
-      "0xc775185C61448F85B2530cc96Ff1297C45AfbF48"
-    ]
-  },
+
+  
   miniapp: {
     version: "1",
     name: "Tic-Tac-Toe", 
     subtitle: "Classic web3 game", 
-    description: "Play TicTacToe built with love by GainChainn.",    screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
+    description: "Play TicTacToe",    screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
     iconUrl: `${ROOT_URL}/blue-icon.png`,
     splashImageUrl: `${ROOT_URL}/blue-hero.png`,
     splashBackgroundColor: "#000000",
     homeUrl: ROOT_URL,
     webhookUrl: `${ROOT_URL}/api/webhook`,
     primaryCategory: "social",
-    tags: ["tic-tac-toe", "web3", "crypto", "base", "game"],
+    tags: ["tic-tac-toe", "game"],
     heroImageUrl: `${ROOT_URL}/blue-hero.png`, 
     tagline: "tic-tac-toe",
     ogTitle: "",
     ogDescription: "",
     ogImageUrl: `${ROOT_URL}/blue-hero.png`,
     "noindex": true
-  }
+  },
+  "baseBuilder": {
+    "allowedAddresses": [
+      "0xc775185C61448F85B2530cc96Ff1297C45AfbF48"
+    ]
+  },
   
 } as const;
 
